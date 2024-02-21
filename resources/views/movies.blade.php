a380482508531778bc92bf8bcc5583daa5fc4fe2
jllloydd <wayfivet@gmail.com>
2024-02-21 12:30:30 +0800

    Update: Usage of data initialized from product controller to view

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -6,7 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Movies</title>
 </head>
-<body>
-    <h1>Goated Movies</h1>
+<body style="display:flex; align-items:center; flex-direction:column; font-family:arial;">
+    <h1 style="text-align:center;">Goated Movies</h1>
+    <ul>
+        <li>{{$topone}}</li>
+        <li>{{$toptwo}}</li>
+        <li>{{$topthree}}</li>
+        <li>{{$topfour}}</li>
+        <li>{{$topfive}}</li>
+    </ul>
 </body>
 </html>
